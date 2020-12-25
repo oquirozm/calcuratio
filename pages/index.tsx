f22765933071a89b735805cc786e801e072ea25b
oquirozm <hello@omar.works>
2020-12-25 18:06:27 +0100

    feat: convert the home page in a 2-column layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import React, { FC, useState } from "react";
-// import ModeSelector from "../components/ModeSelector";
-// import UserInputForm from "../components/UserInputForm";
-import { Title, Text, Flex, Button } from "../components";
-// import { calculateWidth, calculateHeight, calculateAspectRatio } from "utils";
-// import { AspectRatio, Dimensions } from "@types";
+import { Title, Text, Flex, Button, Box } from "../components";
 import { CalculatorForm } from "features";
 
 const Main: FC = () => {
@@ -21,23 +17,28 @@ const Main: FC = () => {
         </Text>
       </Flex>
 
-      {/* <ModeSelector handleModeUpdate={updateMode} mode={mode} />
+      <Flex width="100%">
+        <Flex width={3 / 5} justifyContent="center" alignItems="center">
+          <CalculatorForm
+            clearResult={() => setResult("")}
+            onCalculation={(result: string): void => setResult(result)}
+          />
+        </Flex>
 
-      <Flex flexDirection="column" m={4} alignItems="center">
-        <UserInputForm mode={mode} updateGlobalUserInputs={updateUserInputs} />
-      </Flex> */}
-      <CalculatorForm
-        clearResult={() => setResult("")}
-        onCalculation={(result: string): void => setResult(result)}
-      />
-
-      <Flex flexDirection="column" justifyContent="center" alignItems="center">
-        <Title type="h3" fontSize={30}>
-          Result
-        </Title>
-        <Text type="p" fontSize={45}>
-          {result}
-        </Text>
+        <Flex width={2 / 5} justifyContent="center" alignItems="flex-start">
+          <Flex
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
+            <Title type="h3" fontSize={30} mb={4}>
+              Result
+            </Title>
+            <Text type="p" fontSize={45}>
+              {result}
+            </Text>
+          </Flex>
+        </Flex>
       </Flex>
     </div>
   );
